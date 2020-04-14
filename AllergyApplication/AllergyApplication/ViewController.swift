ccb9c11d3796ca2a68e12c02ec857e7437ff79b7
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2020-04-13 23:33:35 -0700

    the start
    creating the project

---
+++ AllergyApplication/AllergyApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllergyApplication
+//
+//  Created by Melissa Nguyen on 4/13/20.
+//  Copyright © 2020 Melissa Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
